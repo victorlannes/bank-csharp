bec22efa9dc339d4f98327766d4a8fbb1f240a0f
Victor Lannes <victor.ferreiralannes@hotmail.com>
2023-01-17 14:17:18 -0300

    Adicionar arquivos de projeto.

---
+++ ExercBanco/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+
+namespace Curso {
+    class Program {
+        static void Main(string[] args) {
+
+            ContaBancaria conta;
+
+            Console.Write("Entre o número da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.Write("Entre o titular da conta: ");
+            string titular = Console.ReadLine();
+            Console.WriteLine("Haverá depósito inicial (s/n)? ");
+            char resp = char.Parse(Console.ReadLine());
+            if( resp == 's' || resp == 'S') {
+                Console.Write("Entre o valor do depósito inicial: ");
+                double depositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                conta = new ContaBancaria(numero, titular, depositoInicial);
+            } 
+            else {
+                conta = new ContaBancaria(numero, titular);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.WriteLine("Entre um valor para depósito: ");
+            double deposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Deposito(quantia);
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.WriteLine("Entre um valor para saque: ");
+            deposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Saque(quantia);
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(conta);
+        }
+    }
+}
